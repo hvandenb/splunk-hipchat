1183e0299d78205228af92413ed00524701d9809
James Ryan <mail@jamesryan.co.uk>
2015-08-18 19:53:23 +0100

    Version 1.1: Pulled configuration into an external config file. Improved the formatting of alerts in HipChat so that columns and their values are displayed next to each other (better for alerts that return multiple rows). Removed the need for an alert naming convention. Added a configuration setting that allows for the Splunk app to be specified.

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
 # Splunk to HipChat
 
-#### Version 1.0
+#### Version 1.1
 
 This Splunk to Hipchat script is a quick solution to getting Splunk alerts to send to HipChat. 
 It's a bit rough round the edges, but I thought I'd share anyway.
 
 ## Getting started with Splunk to Hipchat
-- Configure the settings in splunkToHipchat.py to match your own Splunk and HipChat configuration and login credentials. 
-The script requires a v2 authentication token to send alerts to HipChat.
-
 - Install Python on your Splunk server. Don't be tempted to rely on the version bundled with Splunk as you will struggle
 to install the Splunk SDK for Python that is needed for this script.
 
@@ -17,22 +14,17 @@ SPLUNK_SCRIPTS_LOCATION should be set to the real path of $SPLUNK_HOME/bin/scrip
 
 - Install the [Splunk Software Development Kit for Python](https://github.com/splunk/splunk-sdk-python) libraries.
 
-- Work out your Splunk alert naming convention. E.g. "PROJECT1_Low_Disk_Space" or "PROJECT1 Low disk space".
-
-- Update the SPLUNK_ALERT_PREFIX_DELIMITER in splunkToHipchat.py to reflect the character you want to use to calculate 
- the PROJECT1 portion of your Splunk alert. By default everything before the first space will be extracted.
+- Update splunkToHipchat.config with your configuration. You should only need to change the first three sections unless you want to customise the alert colours and notifications.
+Default values are offered for some settings but these are probably inappropriate for your configuration.
  
-- Update the mapping between Splunk alert prefixes and the HipChat room names you want to send alerts to in splunkToHipchat.py.
- 
-- Place splunkToHipchat.py and splunkToHipchat.sh into $SPLUNK_HOME/bin/scripts.
+- Place splunkToHipchat.py, splunkToHipchat.sh and splunkToHipchat.config into $SPLUNK_HOME/bin/scripts. Ensure that you have set execute permissions on the shell script. 
 
-- Configure your Splunk alert(s) action to be 'Run a Script' and point them at splunkToHipchat.sh. This acts acts as a wrapper 
- for the Python script to allows you to use your own version of Python. 
+- Configure your Splunk alert(s) action to be 'Run a Script' and point them at splunkToHipchat.sh. This acts acts as a wrapper for the Python script to allows you to use your own version of Python. 
  
 - Wait for an alert to fire and watch as it appears in HipChat.
 
 ## Requirements
-Tested with Python 2.7.6 and Splunk Enterprise 6.2.2 on Linux
+Tested with Python 2.7.6 and Splunk Enterprise 6.2.5 on Linux
 
 ## Dependencies
 The Python script requires the The [Splunk Software Development Kit for Python](https://github.com/splunk/splunk-sdk-python)
--- splunkToHipchat.py
+++ splunkToHipchat.py
@@ -3,6 +3,9 @@ import argparse
 import splunklib.client as client
 import urllib
 import urllib2
+import json
+import ConfigParser
+from collections import OrderedDict
 
 if __name__ == "__main__":
     # Parse input params. These come from the wrapper shell script
@@ -12,58 +15,62 @@ if __name__ == "__main__":
     parser.add_argument('working_directory')
     args = parser.parse_args()
 
+    config = ConfigParser.ConfigParser()
+    config.optionxform = str  # Retains case sensitivity from configuration file
+    config.readfp(open(args.working_directory + '/splunkToHipchat.config'))
+
     # Redirect logs to the same directory as this script lives in
     sys.stdout = open(args.working_directory + '/splunkToHipchat.stdout', 'w')
     sys.stderr = open(args.working_directory + '/splunkToHipchat.stderr', 'w')
 
-    # Configuration
-    SPLUNK_HOST = "localhost"
-    SPLUNK_PORT = 8089
-    SPLUNK_USERNAME = "admin"
-    SPLUNK_PASSWORD = "changeme"
-
-    # The delimiter that follows the name of your project at the front of the Splunk alert.
-    # E.g. set this to "_" if your alert convention is "PROJECT1_LOW_DISK_SPACE" or set it to " " if
-    # your alert is called "Project1 Low Disk Space".
-    SPLUNK_ALERT_PREFIX_DELIMITER = " "
-
-    HIPCHAT_BASE_URL = "https://HIPCHAT_URL"
-    HIPCHAT_AUTH_TOKEN = "YOUR_AUTH_TOKEN"
-    DEFAULT_HIPCHAT_ROOM_NAME = "Splunk alerts with no destination"
-
-    # Splunk alerts map in the following way: Info = 1, Low = 2, Medium = 3, High = 4, Critical = 5
-    SPLUNK_SEVERITY_TO_HIPCHAT_COLOUR_DICT = {"1": "green", "2": "green", "3": "yellow", "4": "red", "5": "red"}
-    SPLUNK_SEVERITY_TO_HIPCHAT_NOTIFY_DICT = {"1": "false", "2": "false", "3": "true", "4": "true", "5": "true"}
-    SPLUNK_ALERT_PREFIX_TO_HIPCHAT_ROOM_DICT = {"PROJECT1": "Project alerts 1",
-                                                "PROJECT2": "Project alerts 2"}
+    # Load configuration into dictionary objects
+    SPLUNK_CONFIG_DICT = dict(config.items('Splunk configuration'))
+    HIPCHAT_CONFIG_DICT = dict(config.items('HipChat configuration'))
+    SPLUNK_SEVERITY_TO_HIPCHAT_COLOUR_DICT = dict(config.items('HipChat alert colour rules'))
+    SPLUNK_SEVERITY_TO_HIPCHAT_NOTIFY_DICT = dict(config.items('HipChat alert notification rules'))
+    SPLUNK_ALERT_PREFIX_TO_HIPCHAT_ROOM_DICT = dict(config.items('Alert name to HipChat room mappings'))
 
     splunk_service = client.connect(
-        host=SPLUNK_HOST,
-        port=SPLUNK_PORT,
-        username=SPLUNK_USERNAME,
-        password=SPLUNK_PASSWORD)
+        host=SPLUNK_CONFIG_DICT.get('host'),
+        port=SPLUNK_CONFIG_DICT.get('port'),
+        username=SPLUNK_CONFIG_DICT.get('username'),
+        password=SPLUNK_CONFIG_DICT.get('password'),
+        app=SPLUNK_CONFIG_DICT.get('app'))
 
     def retrieve_splunk_search_results(url, service):
         job_id = url.split('sid=')[1]
         saved_search_as_job = service.jobs[job_id]
-        search_results = saved_search_as_job.results(**{"output_mode": "csv"})
-        return search_results.read()
+        search_results = saved_search_as_job.results(**{"output_mode": "json"})
+
+        read_search_results = search_results.read()
+        return format_splunk_search_results(read_search_results)
+
+    def format_splunk_search_results(json_splunk_search_results):
+        json_obj = json.loads(json_splunk_search_results, object_pairs_hook=OrderedDict)
+        results_element = json_obj['results']
+
+        results_list = []
+        for dictionary in results_element:
+            for key, value in dictionary.iteritems():
+                results_list.append("\"" + key + ": " + value + "\"")
+
+        return ", ".join(results_list)
 
     def retrieve_splunk_alert_severity(alert_name, service):
         alert_details = service.saved_searches[alert_name]
         return alert_details["alert.severity"]
 
     def retrieve_hipchat_room_from_alert_name(alert_name):
-        splunk_alert_prefix = alert_name.split(SPLUNK_ALERT_PREFIX_DELIMITER, 1)[0]
-        if splunk_alert_prefix in SPLUNK_ALERT_PREFIX_TO_HIPCHAT_ROOM_DICT:
-            hipchat_room = SPLUNK_ALERT_PREFIX_TO_HIPCHAT_ROOM_DICT[splunk_alert_prefix]
-        else:
-            hipchat_room = DEFAULT_HIPCHAT_ROOM_NAME
+        hipchat_room = HIPCHAT_CONFIG_DICT.get('defaultroomname')
+        for key in sorted(SPLUNK_ALERT_PREFIX_TO_HIPCHAT_ROOM_DICT.iterkeys()):
+            if key in alert_name:
+                return SPLUNK_ALERT_PREFIX_TO_HIPCHAT_ROOM_DICT[key]
         return hipchat_room
 
     def send_hipchat_notification(room_id, message, notify, colour):
-        url = urllib.basejoin(HIPCHAT_BASE_URL,
-                              "/v2/room/" + room_id + "/notification?auth_token=" + HIPCHAT_AUTH_TOKEN)
+        url = urllib.basejoin(HIPCHAT_CONFIG_DICT.get('baseurl'),
+                              "/v2/room/" + room_id + "/notification?auth_token=" + HIPCHAT_CONFIG_DICT.get(
+                                  'authtoken'))
         values = {'message': message,
                   'color': colour,
                   'notify': notify}
